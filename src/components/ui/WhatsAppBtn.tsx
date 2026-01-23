541ceaa320ab94f44136bb2d5294883adb04e00a
Enisha <78181175+enisha-op@users.noreply.github.com>
2026-01-23 18:59:00 -0500

    Update WhatsAppBtn.tsx

--- src/components/ui/WhatsAppBtn.tsx
+++ src/components/ui/WhatsAppBtn.tsx
@@ -1,6 +1,6 @@
 export const WhatsAppBtn = () => (
   <a 
-    href="https://wa.me/51949119350?text=Hola%20INOMAC,%20buen%20día.%20Me%20gustaría%20recibir%20más%20información%20sobre%20los%20modelos%20que%20tienen%20disponibles.%20¡Gracias!
+    href="https://wa.me/51949119350?text=Hola%20INOMAC.%20Me%20gustaría%20recibir%20más%20información%20sobre%20los%20modelos%20que%20tienen%20disponibles.%20¡Gracias!
 " 
     target="_blank" 
     className="fixed bottom-8 right-8 z-50 bg-[#25D366] p-4 rounded-full shadow-2xl hover:scale-110 transition-transform"
